9791c0974b8ea69e3681aec1b8443a07668d3829
Aziz-Khaled <94625146+Aziz-Khaled@users.noreply.github.com>
2023-12-07 15:36:17 +0100

    adding first route HomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
